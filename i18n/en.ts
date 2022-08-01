132329189b6361f4bfa26d4d28b6b4d67a17fd0e
muzzik <45589707+1226085293@users.noreply.github.com>
2022-08-01 14:08:15 +0800

    Initial commit

---
+++ i18n/en.ts
@@ -0,0 +1,7 @@
+import type i18n from "../src/i18n";
+
+class en implements i18n {
+	title = "cc-plugin-template";
+}
+
+export = new en();
---
+++ i18n/zh.ts
@@ -0,0 +1,7 @@
+import type i18n from "../src/i18n";
+
+class zh implements i18n {
+	title = "cc-plugin-template";
+}
+
+export = new zh();
